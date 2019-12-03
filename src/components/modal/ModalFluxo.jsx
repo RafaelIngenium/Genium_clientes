daaf5c2242a7e16aa17e9b04f7fb3b80aac1254d
fsaingenium <56600750+fsaingenium@users.noreply.github.com>
2019-12-03 15:54:02 -0300

    ...

--- src/components/modal/ModalFluxo.jsx
+++ src/components/modal/ModalFluxo.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ChannelItem from "../misc/ChannelItem";
 import SelectComponent from "./../misc/SelectComponent";
+import { timeArray } from "../../utils/helpers";
 
 const ModalFluxo = () => {
   const [channels, setChannels] = useState([
@@ -8,6 +9,16 @@ const ModalFluxo = () => {
     { label: "messenger", isChecked: false },
     { label: "whatsapp", isChecked: false },
     { label: "voice", isChecked: false }
+  ]);
+
+  const [activeDays, setActiveDays] = useState([
+    { index: 1, label: "D", active: false },
+    { index: 2, label: "S", active: false },
+    { index: 3, label: "T", active: false },
+    { index: 4, label: "Q", active: false },
+    { index: 5, label: "Q", active: false },
+    { index: 6, label: "S", active: false },
+    { index: 7, label: "S", active: false }
   ]);
 
   const handleChannelClick = channelName => {
@@ -20,32 +31,16 @@ const ModalFluxo = () => {
     setChannels(newChannels);
   };
 
-  const timeArray = [
-    { value: "0", label: "00:00" },
-    { value: "1", label: "01:00" },
-    { value: "2", label: "02:00" },
-    { value: "3", label: "03:00" },
-    { value: "4", label: "04:00" },
-    { value: "5", label: "05:00" },
-    { value: "6", label: "06:00" },
-    { value: "7", label: "07:00" },
-    { value: "8", label: "08:00" },
-    { value: "9", label: "09:00" },
-    { value: "10", label: "10:00" },
-    { value: "11", label: "11:00" },
-    { value: "12", label: "12:00" },
-    { value: "13", label: "13:00" },
-    { value: "14", label: "14:00" },
-    { value: "15", label: "15:00" },
-    { value: "16", label: "16:00" },
-    { value: "17", label: "17:00" },
-    { value: "18", label: "18:00" },
-    { value: "19", label: "19:00" },
-    { value: "20", label: "20:00" },
-    { value: "21", label: "21:00" },
-    { value: "22", label: "22:00" },
-    { value: "23", label: "23:00" }
-  ];
+  const handleDayClick = d => {
+    const newActiveDays = [...activeDays];
+    newActiveDays.map(day => {
+      // console.log(day);
+      if (day.index === d.index) day.active = !day.active;
+    });
+
+    setActiveDays(newActiveDays);
+    console.log(activeDays);
+  };
 
   return (
     <React.Fragment>
@@ -127,13 +122,19 @@ const ModalFluxo = () => {
       </div>
 
       <div className="days-container">
-        <div className="days-container__days-item active">D</div>
-        <div className="days-container__days-item">S</div>
-        <div className="days-container__days-item">T</div>
-        <div className="days-container__days-item">Q</div>
-        <div className="days-container__days-item">Q</div>
-        <div className="days-container__days-item">S</div>
-        <div className="days-container__days-item">S</div>
+        {activeDays.map((day, i) => (
+          <div
+            key={i}
+            className={
+              day.active
+                ? "days-container__days-item active"
+                : "days-container__days-item"
+            }
+            onClick={() => handleDayClick(day)}
+          >
+            {day.label}
+          </div>
+        ))}
       </div>
 
       <div className="select-fluxo select-fluxo--date">
